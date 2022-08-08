f8990a81fa6c89bfc170a4e418d10c88b7a04267
Sonali Singh <singh.sonal@northeastern.edu>
2022-08-08 02:21:43 -0400

    Register User Refactored and Complete

--- src/main/java/com/todo/service/UserService.java
+++ src/main/java/com/todo/service/UserService.java
@@ -1,7 +1,21 @@
 package com.todo.service;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import com.todo.repositories.UserRepository;
 import org.slf4j.Logger;
@@ -38,8 +52,17 @@ public class UserService implements UserInterface {
         newUser.setEmailValidated(false);
         newUser.setCreated_AtTime(new Timestamp(System.currentTimeMillis()));
         newUser.setUpdated_AtTime(new Timestamp(System.currentTimeMillis()));
-        userRepository.save(newUser);
-        return true;
+        try {
+            boolean status = sendEmail(newUser.getEmailAddress());
+            if (status) {
+                userRepository.save(newUser);
+                return true;
+            }
+            return false;
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
@@ -103,14 +126,40 @@ public class UserService implements UserInterface {
         return true;
     }
 
-    private boolean sendEmail(String email) {
+    private boolean sendEmail(String email) throws AddressException, MessagingException, IOException {
         try {
+            String url = "http://localhost:8081/v1/validateEmail?email=" + email;
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
 
+            Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("huskydevportal@gmail.com", "wbppdzxlwdnhlaxa");
+                }
+            });
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("huskydevportal@gmail.com", false));
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            msg.setSubject("Verification Email Link Todo App");
+            msg.setContent(url, "text/html");
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(url, "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            msg.setContent(multipart);
+            Transport.send(msg);
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
+            logger.info("**********Error Sending email !! **********");
             return false;
         }
-        return true;
     }
 
 }
