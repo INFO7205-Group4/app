07c8df3791aa6082d76585711366e5c7277bcd86
Sonali Singh <singh.sonal@northeastern.edu>
2022-08-08 23:44:26 -0400

    Changes

--- src/main/java/com/todo/service/authservice/interceptor/SecurityInterceptor.java
+++ src/main/java/com/todo/service/authservice/interceptor/SecurityInterceptor.java
@@ -6,63 +6,75 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.method.HandlerMethod;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.todo.Interface.AuthServiceInterface;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import com.todo.Interface.NeedLogin;
 
-@Service
+@Component
 public class SecurityInterceptor extends HandlerInterceptorAdapter {
 
-	private final Logger log = LogManager.getLogger(getClass());
-	private static final String AUTH_HEADER_PARAMETER_AUTHERIZATION = "authorization";
-	@Autowired
-	private AuthServiceInterface authService;
+    private final Logger log = LogManager.getLogger(getClass());
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
+    private static final String AUTH_HEADER_PARAMETER_AUTHERIZATION = "authorization";
 
-		Boolean isValidBasicAuthRequest = false;
-		if (handler instanceof HandlerMethod) {
-			NeedLogin needLogin = ((HandlerMethod) handler).getMethodAnnotation(NeedLogin.class);
-			if (null == needLogin) {
-				needLogin = ((HandlerMethod) handler).getMethod().getDeclaringClass()
-						.getAnnotation(NeedLogin.class);
-				isValidBasicAuthRequest = true;
-			}
-			if (null != needLogin) {
-				log.info("[Inside PRE Handle interceptor][" + request + "]" + "[" +
-						request.getMethod() + "]"
-						+ request.getRequestURI());
+    // Get application user name from props.
+    @Value("${app.api.basic.username}")
+    private String userName;
 
-				try {
-					String basicAuthHeaderValue = request.getHeader(AUTH_HEADER_PARAMETER_AUTHERIZATION);
-					isValidBasicAuthRequest = authService.validateBasicAuthentication(basicAuthHeaderValue);
-					if (!isValidBasicAuthRequest) {
-						response.setStatus(HttpStatus.UNAUTHORIZED.value());
-					}
+    // Get application password from props.
+    @Value("${app.api.basic.password}")
+    private String password;
 
-				} catch (Exception e) {
-					log.error("Error occurred while authenticating request : " + e.getMessage());
-					response.setStatus(HttpStatus.UNAUTHORIZED.value());
-				}
-			}
-		}
-		return isValidBasicAuthRequest;
+    @Autowired
+    private AuthServiceInterface authService;
 
-	}
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
+        Boolean isValidBasicAuthRequest = false;
+        if (handler instanceof HandlerMethod) {
+            NeedLogin needLogin = ((HandlerMethod) handler).getMethodAnnotation(NeedLogin.class);
+            if (null == needLogin) {
+                needLogin = ((HandlerMethod) handler).getMethod().getDeclaringClass()
+                        .getAnnotation(NeedLogin.class);
+                isValidBasicAuthRequest = true;
+            }
+            if (null != needLogin) {
 
-		log.info("[Inside POST Handle Interceptor]" + request.getRequestURI());
+                log.info("[Inside PRE Handle interceptor][" + request + "]" + "[" + request.getMethod() + "]"
+                        + request.getRequestURI());
 
-	}
+                try {
 
+                    // Grab basic header value from request header object.
+                    String basicAuthHeaderValue = request.getHeader(AUTH_HEADER_PARAMETER_AUTHERIZATION);
+
+                    // Process basic authentication
+                    isValidBasicAuthRequest = authService.validateBasicAuthentication(basicAuthHeaderValue);
+
+                    // If this is invalid request, then set the status as UNAUTHORIZED.
+                    if (!isValidBasicAuthRequest) {
+                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                    }
+
+                } catch (Exception e) {
+                    log.error("Error occured while authenticating request : " + e.getMessage());
+                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                }
+            }
+        }
+        return isValidBasicAuthRequest;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+    }
 }
